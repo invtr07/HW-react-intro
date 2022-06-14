ae7829d28fb9c2f73d9c1fd727832bda94365df7
invtr07 <95737990+invtr07@users.noreply.github.com>
2022-06-14 21:42:45 +0600

    update of code

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,10 +2,32 @@ import './App.css';
 
 function App() {
   //it is very important to set correct path to your file!!!!!!
-  const image1= './images/photo-1.jpeg';
+
+  const image= ['./images/photo-1.jpeg','./images/photo-2.jpeg','./images/photo-3.jpeg','./images/photo-4.jpeg','./images/photo-5.jpeg'];
+
   return (
     <div className ="App">
-        <img alt="photo1" src={image1} />
+      <header>
+        Photoes pro
+      </header>
+      <div className='container'>
+        <div className='img-txt'>
+          <img className="photo" alt="photo1" src={image[0]} />
+        </div>
+        <div className='img-txt'>
+          <img className="photo" alt="photo1" src={image[1]} />
+        </div>
+        <div className='img-txt'>
+          <img className="photo" alt="photo1" src={image[2]} />
+        </div>
+        <div className='img-txt'>
+          <img className="photo" alt="photo1" src={image[3]} />
+        </div>
+        <div className='img-txt'>
+          <img className="photo" alt="photo1" src={image[4]} />
+        </div>
+        
+      </div>    
     </div>  
   );
 }
